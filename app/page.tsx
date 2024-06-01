03bc8421de3b9a9093fae24e7fc54f7e8d046ba1
wonglok <yellowhappy831@gmail.com>
2024-06-01 15:33:44 +0800

    yo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div>
-      <div>123</div>
+    <div className="w-full h-full">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
